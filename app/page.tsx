8f42c765e16d0738bff85c19725aa6806c235bcd
roshanmandal45 <kumarroshan1807@gmail.com>
2026-08-01 17:44:30 +0545

    PopularRoom section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Homepage from './components/Homepage'
 import Navbar from './components/Navbar'
+import PopularRooms from './components/PopularRoom'
 
 export default function page() {
   return (
     <main>
       <Navbar/>
       <Homepage/>
+      <PopularRooms/>
     </main>
   )
 }
