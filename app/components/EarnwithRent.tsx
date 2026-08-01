f6a064f9d61bcde295899cf174b20c3c762238c7
roshanmandal45 <kumarroshan1807@gmail.com>
2026-08-01 21:12:00 +0545

    sticky nav and transparent

---
+++ app/components/EarnwithRent.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function EarnwithRent() {
+  return (
+    <section>
+        <h1>
+            Earn with Room Sewa
+        </h1>
+        <p>
+            List your property and connect with renters actively searching for a place.<br/>
+             Earn through visits, bookings, and commissions with clear support at every step.
+        </p>
+    </section>
+  )
+}
