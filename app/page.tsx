8a85ab75747af80e54ff9d2102b28ae2849753e9
roshanmandal45 <kumarroshan1807@gmail.com>
2026-08-01 10:44:40 +0545

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Homepage from './components/Homepage'
+import Navbar from './components/Navbar'
 
 export default function page() {
   return (
     <main>
+      <Navbar/>
       <Homepage/>
     </main>
   )
